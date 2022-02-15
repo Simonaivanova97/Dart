d3df55a481c866b74c9c786484e9cb3931b09b31
Симона Иванова <simona02ivanova@gmail.com>
2022-02-15 15:29:55 +0200

    Add solution task2

---
+++ Loops/task2.dart
@@ -0,0 +1,9 @@
+/* Напишете for цикъл, започващ от 1 до 10 включително. Изведете квадратите на всяко число. */
+import 'package:dartExercise/dartExercise.dart' as dartExercise;
+
+int main(){
+  for(int i=1;i<=10;i++){
+    print(i*i);
+  }
+  return 0;
+}
